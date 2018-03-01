40b29a1739134f545ad4f635d03b9c9d4c31bc7e
EC2 Default User <ec2-user@ip-172-31-6-74.ap-southeast-2.compute.internal>
2018-03-01 20:01:10 +0000

    roa says no

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+pip-3.6 install django
+rem django-admin startproject roa
+pushd roa
+rem python3 manage.py createsuperuser --username=admin2 --email=nitrologic@gmail.com
+rem python3 manage.py startapp junrei
+popd
---
+++ runserver.sh
@@ -0,0 +1,1 @@
+python3 roa/manage.py runserver 0.0.0.0:8080

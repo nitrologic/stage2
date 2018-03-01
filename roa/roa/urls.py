57b9285dc9290191d62028660e1611ea1da9851b
Simon Armstrong <nitrologic@gmail.com>
2018-03-02 09:08:14 +1300

    fix roa

--- roa/roa/urls.py
+++ roa/roa/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic import RedirectView
+from django.urls import path
 from django.conf.urls import url,include
 from django.contrib import admin
 

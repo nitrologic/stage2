2dfbc5905be37c84b2fdd502ec58251febf7bf7b
Simon Armstrong <nitrologic@gmail.com>
2018-03-02 19:23:08 +1300

    tidy

--- roa/static/stage2.js
+++ roa/static/stage2.js
@@ -13,39 +13,17 @@ var extent = Cesium.Rectangle.fromCartesianArray(home);
 Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
 Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
 
-var time=new Cesium.JulianDate(0,60*60*15);
-var dragontime=new Cesium.JulianDate(0,60*60*8);
-
-var editor = new Cesium.Viewer('editor', {
-  animation: false,
-  baseLayerPicker : false,
-  fullscreenButton: false,
-  geocoder:false,  
-  homeButton: false,
-  timeline: false,
-  infoBox: false,
-  sceneModePicker: false,
-  selectionIndicator: false,
-  navigationHelpButton: false,
-  creditContainer: "credits",
-  sceneMode : Cesium.SceneMode.COLUMBUS_VIEW,
-  imageryProvider : Cesium.createOpenStreetMapImageryProvider({url : 'https://a.tile.openstreetmap.org/'})
-  });
-  
-/*
-  var clock = new Cesium.Clock();
-  var clockViewModel = new Cesium.ClockViewModel(clock);
-  var viewModel = new Cesium.AnimationViewModel(clockViewModel);
-  var widget = new Cesium.Animation('jog', viewModel);
-*/
+var time=new Cesium.JulianDate(0,60*60*14);
 
 var terrainSource="https://s3-ap-southeast-2.amazonaws.com/roanz/terrain/nz-dem";
-var aerialSource="https://s3-ap-southeast-2.amazonaws.com/roanz/aerial/marble2004-sat";
+var baseSource="https://s3-ap-southeast-2.amazonaws.com/roanz/aerial/marble2004-sat";
+var aerialSource="https://s3-ap-southeast-2.amazonaws.com/roanz/aerial/aerial.json";
 
 var viewer = new Cesium.Viewer('viewer', {
   sceneMode : Cesium.SceneMode.SCENE_3D,
+//  sceneMode :   Cesium.SceneMode.COLUMBUS_VIEW,
   mapProjection : new Cesium.WebMercatorProjection(),
-  imageryProvider : Cesium.createOpenStreetMapImageryProvider({url : aerialSource}),
+  imageryProvider : Cesium.createOpenStreetMapImageryProvider({url : baseSource, maximumLevel:8 }),
   terrainProvider : new Cesium.CesiumTerrainProvider({url : terrainSource, requestVertexNormals: true}),
   baseLayerPicker : false,
   homeButton: false,
@@ -55,36 +33,26 @@ var viewer = new Cesium.Viewer('viewer', {
   sceneModePicker: false,
   selectionIndicator: false,
   navigationHelpButton: false,
-  shadows : true,
-  vrButton:true,
+  shadows : false,
+  vrButton:false,
   creditContainer: "credits",
   animation: false,
-  // clockViewModel:clockViewModel
-//    terrainShadows : Cesium.ShadowMode.ENABLED,    
-//    automaticallyTrackDataSourceClocks: false,
-//    mapProjection : new Cesium.WebMercatorProjection()
 });
 
 var shadows = viewer.shadowMap;
 shadows.darkness=0.86;
 
 var scene = viewer.scene;
-
 scene.globe.enableLighting = true;
+scene.fog.enable=false;
 scene.allowTextureFilterAnisotropic = true;
+
 viewer.clock.currentTime = time;
 
 // viewer.clock.multiplier = 2000;
-scene.globe.maximumScreenSpaceError = 2;
+scene.globe.maximumScreenSpaceError = 1.2;
 
 //viewer.clockViewModel= new Cesium.ClockViewModel(viewer.clock);
-
-var canvas = viewer.canvas;
-canvas.setAttribute('tabindex', '0'); // needed to put focus on the canvas
-canvas.onclick = function() {
-    canvas.focus();
-};
-var ellipsoid = viewer.scene.globe.ellipsoid;
 
 /*
 scene.screenSpaceCameraController.enableTilt = true;
@@ -101,9 +69,6 @@ var placemat= new Cesium.GridMaterialProperty({
   lineCount : new Cesium.Cartesian2(16,16),
   lineThickness : new Cesium.Cartesian2(0.4, 0.4)
 });
-
-
-//36°50′26″S 174°44′24″E 'dragon.json'
 
 var tiles=[];
 var tilesrc=[];
@@ -151,6 +116,7 @@ var cursor = viewer.entities.add({
 viewer.zoomTo(cursor,range);
 
 viewer.extend(Cesium.viewerDragDropMixin);
+
 viewer.dropError.addEventListener(function(dropHandler, name, error) {
 console.log(error);
 window.alert(error);
@@ -168,4 +134,122 @@ var flags = {
     moveLeft : false,
     moveRight : false
 };
+*/
+
+var canvas = viewer.canvas;
+canvas.setAttribute('tabindex', '0'); // needed to put focus on the canvas
+canvas.onclick = function() {
+    canvas.focus();
+};
+
+var ellipsoid = viewer.scene.globe.ellipsoid;
+
+/*
+// disable the default event handlers
+scene.screenSpaceCameraController.enableRotate = false;
+scene.screenSpaceCameraController.enableTranslate = false;
+scene.screenSpaceCameraController.enableZoom = false;
+scene.screenSpaceCameraController.enableTilt = false;
+scene.screenSpaceCameraController.enableLook = false;
+
+var startMousePosition;
+var mousePosition;
+var flags = {
+    looking : false,
+    moveForward : false,
+    moveBackward : false,
+    moveUp : false,
+    moveDown : false,
+    moveLeft : false,
+    moveRight : false
+};
+
+var handler = new Cesium.ScreenSpaceEventHandler(canvas);
+
+handler.setInputAction(function(movement) {
+    flags.looking = true;
+    mousePosition = startMousePosition = Cesium.Cartesian3.clone(movement.position);
+}, Cesium.ScreenSpaceEventType.LEFT_DOWN);
+
+handler.setInputAction(function(movement) {
+    mousePosition = movement.endPosition;
+}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+handler.setInputAction(function(position) {
+    flags.looking = false;
+}, Cesium.ScreenSpaceEventType.LEFT_UP);
+
+function getFlagForKeyCode(keyCode) {
+    switch (keyCode) {
+    case 'W'.charCodeAt(0):
+        return 'moveForward';
+    case 'S'.charCodeAt(0):
+        return 'moveBackward';
+    case 'Q'.charCodeAt(0):
+        return 'moveUp';
+    case 'E'.charCodeAt(0):
+        return 'moveDown';
+    case 'D'.charCodeAt(0):
+        return 'moveRight';
+    case 'A'.charCodeAt(0):
+        return 'moveLeft';
+    default:
+        return undefined;
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    var flagName = getFlagForKeyCode(e.keyCode);
+    if (typeof flagName !== 'undefined') {
+        flags[flagName] = true;
+    }
+}, false);
+
+document.addEventListener('keyup', function(e) {
+    var flagName = getFlagForKeyCode(e.keyCode);
+    if (typeof flagName !== 'undefined') {
+        flags[flagName] = false;
+    }
+}, false);
+
+viewer.clock.onTick.addEventListener(function(clock) {
+    var camera = viewer.camera;
+
+    if (flags.looking) {
+        var width = canvas.clientWidth;
+        var height = canvas.clientHeight;
+
+        // Coordinate (0.0, 0.0) will be where the mouse was clicked.
+        var x = (mousePosition.x - startMousePosition.x) / width;
+        var y = -(mousePosition.y - startMousePosition.y) / height;
+
+        var lookFactor = 0.05;
+        camera.lookRight(x * lookFactor);
+        camera.lookUp(y * lookFactor);
+    }
+
+    // Change movement speed based on the distance of the camera to the surface of the ellipsoid.
+    var cameraHeight = ellipsoid.cartesianToCartographic(camera.position).height;
+    var moveRate = cameraHeight / 100.0;
+
+    if (flags.moveForward) {
+        camera.moveForward(moveRate);
+    }
+    if (flags.moveBackward) {
+        camera.moveBackward(moveRate);
+    }
+    if (flags.moveUp) {
+        camera.moveUp(moveRate);
+    }
+    if (flags.moveDown) {
+        camera.moveDown(moveRate);
+    }
+    if (flags.moveLeft) {
+        camera.moveLeft(moveRate);
+    }
+    if (flags.moveRight) {
+        camera.moveRight(moveRate);
+    }
+});
+
 */

751a2befb41a08c378c85ad46b5ebe96a0d8c76f
Simon Armstrong <nitrologic@gmail.com>
2018-05-07 09:38:45 +1200

    bump hosts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-pip-3.6 install django
+sudo apt install python3-pip
+pip3 install django
 rem django-admin startproject roa
 pushd roa
 rem python3 manage.py createsuperuser --username=admin2 --email=nitrologic@gmail.com
